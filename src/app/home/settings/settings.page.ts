9b3486a27404b46d995ac76257d45d8e6f5031ae
Khairul Azry <mkhairulazry@gmail.com>
2025-07-21 00:53:32 +0800

    Add content in Settings page
    • Upload excel sheet
    • Add questions

--- src/app/home/settings/settings.page.ts
+++ src/app/home/settings/settings.page.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonBackButton,
+    IonButtons,
+    IonContent, 
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonList, 
+    IonTitle, 
+    IonToolbar, 
+  ]
 })
 export class SettingsPage implements OnInit {
 
